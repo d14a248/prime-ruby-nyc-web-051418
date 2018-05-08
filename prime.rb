0ee91160d19bce5df8a35548131f4b2ab3373f71
david landau <thelandaus@gmail.com>
2018-05-08 03:33:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,9 @@ def prime?(int)
     return false
   else
     (2..int).to_a.each do |num|
-      if num % i == 0:
+      if num % i == 0
         return false
+      end
       return True
+  end
 end
